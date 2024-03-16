953b6fb27e1f810e04f483df10155777493441a4
Ezeabasili Edward <81073568+EDwith2ds@users.noreply.github.com>
2024-03-16 15:11:15 +0100

    added todo for the humspider

--- hummus/spiders/humspider.py
+++ hummus/spiders/humspider.py
@@ -16,3 +16,9 @@ class HumspiderSpider(CrawlSpider):
         #item["name"] = response.xpath('//div[@id="name"]').get()
         #item["description"] = response.xpath('//div[@id="description"]').get()
         return item
+
+#TODO: find css seelctors for the name, rating/ rank, score  
+
+#TODO: idea is to combine different anime scores and get the plots too, just incase...
+    
+#TODO: find out if you can have different spiders for the same crawler[reduce stress wehn extracting animes]
