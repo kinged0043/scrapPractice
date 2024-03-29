dd8e495db535b9fe7b3caa19090544a08013cb95
ezeabasili edward <ezeabasilied15@gmail.com>
2024-03-29 13:30:27 +0100

    updated git ignore and humspider file

--- hummus/spiders/humspider.py
+++ hummus/spiders/humspider.py
@@ -5,6 +5,9 @@ from scrapy.loader import ItemLoader
 from ..items import HummusItem
 
 class HumspiderSpider(CrawlSpider):
+    custom_settings = {"CLOSEDSPIDER_ITEMCOUNT": 250,
+                       "CLOSEDSPIDER_PAGECOUNT": 2 } # hopefully this limits the number of scraped items
+
     name = "humspider"
     allowed_domains = ["myanimelist.net"]
     start_urls = ["https://myanimelist.net/topanime.php?type=airing"]
@@ -14,33 +17,25 @@ class HumspiderSpider(CrawlSpider):
                )
 
     def parse_item(self, response):
-        # load = ItemLoader(item=I temLoader(), response=response)
-        # load.add_css("rank", "")
-        # load.add_css("rank", "")
-        # load.add_css("rank", "")
-        # load.add_css("rank", "")
-        # load.add_css("rank", "")
-        # return load.load_item()
+        load = ItemLoader(item=HummusItem(), response=response)
+        load.add_css("title", "div[itemprop=name] p.title-english.title-inherit")
+        load.add_css("score", "div.score-label.score-8")
+        load.add_css("season", "div.information-block.di-ib.clearfix span.information.season a")
+        load.add_css("type", "div.information-block.di-ib.clearfix span.information.type a")
+        load.add_css("plot", "td[valign=top] p[itemprop=description]")
+        return load.load_item()
         """
-        1."div[itemprop=name] p.title-english.title-inherit::text" (anime english name)
+        1."::text" (anime english name)
             query the element > space for searching for element > add . when theres is space btw class or id > ::text to get the text of said element
-        2. div.score-label.score-8::text (anime score)
+        2. ::text (anime score)
 
-        3. div.information-block.di-ib.clearfix span.information.season a::text ( season)
+        3. ::text ( season)
 
-        4. div.information-block.di-ib.clearfix span.information.type a::text" (type)
+        4. ::text" (type)
 
-        5. td[valign=top] p[itemprop=description]::text (anime plot)
+        5. ::text (anime plot)
         """
-        yield {
-            'rank': response.css("tr.ranking-list span.lightLink.top-anime-rank-text.rank1::text"),
-            'title': response.css("div[itemprop=name] p.title-english.title-inherit::text").get(),
-            'score': response.css("div.score-label.score-8::text").get(),
-            'season': response.css("div.information-block.di-ib.clearfix span.information.season a::text").get(),
-            'type': response.css("div.information-block.di-ib.clearfix span.information.type a::text").get(),
-            'plot': response.css("td[valign=top] p[itemprop=description]::text").get()
-            }
-
+      
 #TODO: find css seelctors for the name, rating/ rank, score  /DONE
 
 #TODO: idea is to combine different anime scores and get the plots too, just incase... /DONE
